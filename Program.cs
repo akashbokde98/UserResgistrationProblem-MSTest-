03b29fb290d819e8231120238b734b909fe8a913
akashbokde98 <akashbokde98@gmail.com>
2023-02-17 12:11:43 +0530

    Added: Test Email Samples

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@ namespace UserRegistration
 {
     public class Program
     {
-        public static string Password_4Pattern = @"^((?=.*\d)(?=.*\W)(?=.*[A-Z])(?=.*[a-z]).{8,})$";
+        public static string EmailAddressSamples = @"^[a-z]{3,}[.]?[-+]*[0-9]*[@]{1,1}[a-z0-9]{1,5}[.]{1,1}[a-z]{3,3}[,]?[.]?[a-z]*$";
         static void Main(string[] args)
         {
             Console.WriteLine("User Registration Problem!");
         }
-        public bool CheckPassword_4(string input)
+        public bool CheckEmailSamples(string input)
         {
-            Regex rg = new Regex(Password_4Pattern);
+            Regex rg = new Regex(EmailAddressSamples);
             return rg.IsMatch(input);
         }
     }
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -11,16 +11,22 @@ namespace UnitTest
         }
 
         [Test]
-        [TestCase("A@dfesf9")]
-        [TestCase("0daSkks#lk")]
-        [TestCase("aka$H8okde")]
-        [TestCase("A4sdfa02kglafg")]
-        public void CheckPassword_4_PassingValidInput_ShouldReturnTrue(string userInput)
+        [TestCase("abc@yahoo.com,")]
+        [TestCase("abc-100@yahoo.com,")]
+        [TestCase("abc.100@yahoo.com")]
+        [TestCase("abc111@abc.com,")]
+        [TestCase("abc-100@abc.net,")]
+        [TestCase("abc.100@abc.com.au")]
+        [TestCase("abc@1.com,")]
+        [TestCase("abc@gmail.com.com")]
+        [TestCase("abc+100@gmail.com")]
+
+        public void CheckEmailSamples_PassingValidInput_ShouldReturnTrue(string userInput)
         {
             Program name = new Program();
             bool expected = true;
 
-            var actual = name.CheckPassword_4(userInput);
+            var actual = name.CheckEmailSamples(userInput);
 
             Assert.AreEqual(expected, actual);
         }
